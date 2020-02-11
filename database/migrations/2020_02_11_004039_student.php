df1ec33b1ce7030057e8d9ef65dcba45afa2f243
Hugo <hugoarpaulista@gmail.com>
2020-02-10 21:57:49 -0300

    instalação do adminlte, criacao do banco e das migrations

---
+++ database/migrations/2020_02_11_004039_student.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Student extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('person_id');
+            $table->foreign('person_id')
+                  ->references('id')
+                  ->on('person')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
---
+++ database/migrations/2020_02_11_004052_team.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Adotei o nome de Team pois o nome Class iria conflitar com
+ * a palavra reservada do php para definir classes
+ */
+
+class Team extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team');
+    }
+}
